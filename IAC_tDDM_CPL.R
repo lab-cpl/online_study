18333ba9bb027f83aa37c7d070e2a7bda645bdff
nicolasluarte <l.luarte@uc.cl>
2023-12-10 17:59:31 -0300

    binned + zscores

--- IAC_tDDM_CPL.R
+++ IAC_tDDM_CPL.R
@@ -40,7 +40,7 @@ sourceCpp("tSSM_Rcpp.cpp")
 # this csv contains health and taste difference computed as healthy option - unhealthy options
 # when both options were rated equally they were removed
 # healthy option is the one with highest health rating
-dataBeh <- read_csv("filtered_data_for_DDM_with_health_as_reference.csv") %>% 
+raw <- read_csv("filtered_data_for_DDM_with_health_as_reference.csv") %>% 
     dplyr::rename(
         td = dTaste,
         hd = dHealth,
@@ -52,6 +52,23 @@ dataBeh <- read_csv("filtered_data_for_DDM_with_health_as_reference.csv") %>%
         rt = rt / 1000 # use second instead of ms
     )
 
+# data pre-proc
+
+# step 1: 10 equally spaced bins for health and taste ratings
+# step 2: z-score those bins
+# step 3: signed difference from z-scores (healthy - unhealthy)
+dataBeh <-
+    raw %>% 
+    mutate(
+        bin10_healthIzq = as.numeric(cut(healthIzq, breaks = 10, labels = 1:10)) %>% scale(),
+        bin10_healthDer = as.numeric(cut(healthDer, breaks = 10, labels = 1:10)) %>% scale(),
+        bin10_tasteIzq  = as.numeric(cut(tasteIzq, breaks = 10, labels = 1:10)) %>% scale(),
+        bin10_tasteDer  = as.numeric(cut(tasteDer, breaks = 10, labels = 1:10)) %>% scale(),
+        bin_hd = if_else(bin10_healthIzq > bin10_healthDer, bin10_healthIzq - bin10_healthDer, bin10_healthDer - bin10_healthIzq),
+        bin_td = if_else(bin10_tasteIzq > bin10_tasteDer, bin10_tasteIzq - bin10_tasteDer, bin10_tasteDer - bin10_tasteIzq),
+        unidentifiable = if_else(healthIzq == healthDer | tasteIzq == tasteDer, 1, 0)
+    )
+
 dataBeh$abstd = abs(dataBeh$td)
 dataBeh$abshd = abs(dataBeh$hd)
 dataBeh$logRT = log(dataBeh$rt)
@@ -60,7 +77,8 @@ dataBeh$logRT = log(dataBeh$rt)
 dataBeh <- dataBeh %>% 
     mutate(
         choseL = if_else(sub_healthy_choice == "healthy", 1, 0),
-        RTddm = if_else(choseL == 0, rt * -1, rt) # if unhealthy is choosen then rt is negative
+        # if unhealthy is choosen then rt is negative
+        RTddm = if_else(choseL == 0, rt * -1, rt)
     )
 ntrials = length(dataBeh$rt)
 
@@ -100,7 +118,7 @@ ll_ddm2 <- function(x, dataBeh2, vd, hd) {
     rts = ddm2_parallel(d_v,d_h,thres,nDT,tHin,bias,vd[i],hd[i],3000) # 3000 is the number of simulated RTs
     rts = rts[rts!=0]
     xdens = density(rts, from=-5, to=5, n=1024, bw=0.11) #matches the prob. space of RTs from above
-    idx = which(dataBeh2$td==vd[i] & dataBeh2$hd==hd[i])
+    idx = which(dataBeh2$bin_td==vd[i] & dataBeh2$bin_hd==hd[i])
     # this is a vector of probabilities for all trial with same taste and health difference
     probs = c(probs, dt*xdens$y[dataBeh2$RTddm_pos[idx]])
   }
@@ -122,19 +140,25 @@ fitSub <- function(s, dataBeh) {
   #data1 = ddply(dataBeh2, .(td, hd), summarize, acc= mean(choseL))
   # td and hd were changes to 10 equally spaced bins
   # this should reduce computation time
-  data1 = ddply(dataBeh2, .(bin_taste, bin_health), summarize, acc= mean(choseL))
+  data1 = ddply(dataBeh2, .(bin_td, bin_hd), summarize, acc= mean(choseL))
   
   #vd = data1$td # value difference for attribute 1 (here: taste)
   #hd = data1$hd # value difference for attribute 2 (here: health)
   
   # binned differences for taste and health
-  vd = data1$bin_taste
-  hd = data1$bin_health
+  vd = data1$bin_td
+  hd = data1$bin_hd
   
   # boundaries for parameters
-  lower <- c(-2,-2,0.6,0.01,-1,-1)
-  upper <- c(2,2,3,1,1,1)
+  # test increasing boundaries 
+  # lower <- c(-2,-2,0.6,0.01,-1,-1)
+  # upper <- c(2,2,3,1,1,1)
   
+  lower <- c(-4,  -4, 0.6, 0.01, -4, -1)
+  upper <- c( 4,   4, 3,   1,     4,  1)
+  
+  # force the optimizer for a given number of iterations
+  # get loglikelihood of every iteration per subject
   fit_s = DEoptim(ll_ddm2, lower, upper, DEoptim.control(itermax = 150), dataBeh2=dataBeh2, vd=vd, hd=hd)
   fits[1,1:6] = fit_s$optim$bestmem #fitted parameters
   fits[1,7] = fit_s$optim$bestval #LL
@@ -144,12 +168,14 @@ fitSub <- function(s, dataBeh) {
   f = as.data.frame(matrix(unlist(fits),ncol=9,byrow=TRUE))
   names(f)<-c("d_t", "d_h", "thres", "nDT", "timeHin", "bias", "LL", "BIC", "AIC")
   write_csv(f, paste0(label, ".csv"))
+  saveRDS(fits_s, paste0(label, "_diagnostics", ".rds"))
   return(fits)
 }
 
 inputs = 1:length(unique(dataBeh$subID))
 numCores <- detectCores()
 fits = pbmcapply::pbmclapply(inputs, fitSub, mc.cores = numCores, dataBeh=dataBeh)
+fitSub(1, dataBeh)
 
 #save now in case unlist fails
 fitsF=fits
